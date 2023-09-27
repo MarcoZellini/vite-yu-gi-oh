0310d810d0ecaabe49d4437b94624a344aebec7a
Marco Zellini <zellinimarco@gmail.com>
2023-09-27 14:10:36 +0200

    Add CardList Component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,12 @@
 <script>
 import { store } from '../store.js'
-import CardItem from './CardItem.vue'
+import CardList from './CardList.vue'
 import AppFilter from './AppFilter.vue'
 
 export default {
     name: 'AppMain',
     components: {
-        CardItem,
+        CardList,
         AppFilter
     },
     data() {
@@ -42,16 +42,7 @@ export default {
 
             <AppFilter @change-selected="filterData" />
 
-            <div v-if="store.cardCounter > 0"
-                class="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-3">
-                <div class="search_info">Found {{ store.cardCounter }} cards</div>
-                <div class="col" v-for="card in store.data">
-                    <CardItem :card="card" />
-                </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row -->
-            <div v-else class="page_loader"></div>
+            <CardList />
         </div>
         <!-- /.container -->
     </main>
---
+++ src/components/CardList.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from '../store.js'
+import CardItem from './CardItem.vue'
+
+export default {
+    name: 'CardList',
+    components: {
+        CardItem
+    },
+    data() {
+        return {
+            store
+        }
+    }
+}
+</script>
+
+<template>
+    <div v-if="store.cardCounter > 0"
+        class="row justify-content-center row-cols-1row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-5 g-3">
+        <div class="search_info">Found {{ store.cardCounter }} cards</div>
+        <div class="col" v-for="card in store.data">
+            <CardItem :card="card" />
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+    <div v-else class="page_loader"></div>
+</template>
+
+<style></style>
